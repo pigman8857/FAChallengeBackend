285ad75cdb1a0e3ba15340c5ab63b2d8af33f533
OhSva <somkamolv@hotmail.com>
2020-09-13 21:54:22 +0700

    add final migration

---
+++ WebAPI/Migrations/20200913145246_Final.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class Final : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentDetails");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentDetails",
+                columns: table => new
+                {
+                    PMId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CVV = table.Column<string>(type: "nvarchar(3)", nullable: false),
+                    CardNumber = table.Column<string>(type: "nvarchar(16)", nullable: false),
+                    CardOwnerName = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    ExpirationDate = table.Column<string>(type: "nvarchar(16)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentDetails", x => x.PMId);
+                });
+        }
+    }
+}
